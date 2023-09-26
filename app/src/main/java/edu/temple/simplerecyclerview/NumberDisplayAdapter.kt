f2349fda1c6f458fd8531394b91446365cd53bdb
tesfie056 <143583774+tesfie056@users.noreply.github.com>
2023-09-26 15:08:30 -0400

    extended the RecyclerView inplemented three functions: OncreatedViewHolder, getItemCount, onBindViewolder

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,13 +1,36 @@
 package edu.temple.simplerecyclerview
 
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter(_number: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() /* Step 3a: Provide Adapter Parent */ {
 
-    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+
+    private val number = _number
+    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {
+        val numberView = textView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+      return NumberViewHolder(
+          TextView(
+              parent.context
+          ).apply {
+              layoutParams = 
+          }
+      )
+    }
+
+    override fun getItemCount(): Int {
+        return number.size
+    }
+
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+
+    }
 
     //Step 3b: Complete function definitions for adapter
 
